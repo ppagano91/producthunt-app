b6ebd7c16e20ea0793453c22f1a983f0f50e928e
Patricio Pagano <patricio.pagano@geosystems.com.ar>
2023-08-15 09:44:59 -0300

    Creando formulario para página Crear Cuenta

--- pages/crear-cuenta.js
+++ pages/crear-cuenta.js
@@ -5,7 +5,39 @@ const CrearCuenta = () => {
   return (
     <div>
       <Layout>
-        <h1>Crear Cuenta</h1>
+        <>
+          <h1>Crear Cuenta</h1>
+          <form>
+            <div>
+              <label htmlFor="nombre">Nombre</label>
+              <input
+                type="text"
+                id="nombre"
+                placeholder="Tu Nombre"
+                name="nombre"
+              />
+            </div>
+            <div>
+              <label htmlFor="email">Email</label>
+              <input
+                type="text"
+                id="email"
+                placeholder="Tu email"
+                name="email"
+              />
+            </div>
+            <div>
+              <label htmlFor="password">Password</label>
+              <input
+                type="text"
+                id="password"
+                placeholder="Tu password"
+                name="password"
+              />
+            </div>
+            <input type="submit" value="Crear Cuenta" />
+          </form>
+        </>
       </Layout>
     </div>
   );
