0ef0eb7ee436ccde0ef6266c6e48fe2ac72b52e9
Patricio Pagano <patricio.pagano@geosystems.com.ar>
2023-08-14 22:48:52 -0300

    Estilos de Componentes Header y Navbar

--- components/layout/Header.js
+++ components/layout/Header.js
@@ -1,23 +1,83 @@
 import React from "react";
-import Buscar from "../user-interface/Buscar";
 import Navegacion from "./Navegacion";
 import Link from "next/link";
+import styled from "@emotion/styled";
+import { css } from "@emotion/react";
+import Buscar from "../user-interface/Buscar";
+import Boton from "../user-interface/Boton";
+
+const ContenedorHeader = styled.div`
+  max-width: 1200px;
+  width: 95%;
+  margin: 0 auto;
+  @media (min-width: 768px) {
+    display: flex;
+    justify-content: space-between;
+  }
+`;
+
+const Logo = styled.p`
+  color: var(--naranja);
+  font-size: 4rem;
+  line-height: 0;
+  font-weight: 700;
+  font-family: "Roboto Slab", serif;
+  margin-right: 2rem;
+`;
+
+const usuario = false;
 
 const Header = () => {
   return (
-    <header>
-      <div>
-        <div>
+    <header
+      css={css`
+        border-bottom: 2px solid var(--gris3);
+        padding: 1rem 0;
+      `}
+    >
+      <ContenedorHeader>
+        <div
+          css={css`
+            display: flex;
+            align-items: center;
+          `}
+        >
+          <Link href="/">
+            <Logo>P</Logo>
+          </Link>
+
           <Buscar />
           <Navegacion />
         </div>
-        <div>
-          <p>Hola Patricio!</p>
-          <button type="button">Cerrar Sesión</button>
-          <Link href="/">Login</Link>
-          <Link href="/">Crear Cuenta</Link>
+        <div
+          css={css`
+            display: flex;
+            align-items: center;
+          `}
+        >
+          {usuario ? (
+            <>
+              <p
+                css={css`
+                  margin-right: 2rem;
+                `}
+              >
+                Hola Patricio!
+              </p>
+              <Boton bgColor="true">Cerrar Sesión</Boton>
+            </>
+          ) : (
+            <>
+              <Link href="/login">
+                <Boton bgColor="true">Login</Boton>
+              </Link>
+              <Link href="/crear-cuenta">
+                <Boton>Crear Cuenta</Boton>
+              </Link>
+            </>
+          )}
         </div>
-      </div>
+      </ContenedorHeader>
     </header>
   );
 };
