6afa3522669b273bca5f6c85e01ebf8458026ecd
Patricio Pagano <patricio.pagano@geosystems.com.ar>
2023-08-23 11:21:00 -0300

    Consultando la Base de Datos

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,41 @@
-import React from 'react'
-import Layout from '../components/layout/Layout'
-
+import React, { useState, useEffect, useContext } from "react";
+import Layout from "../components/layout/Layout";
+import { FirebaseContext } from "../firebase";
 
 export default function Home() {
+  const [productos, setProductos] = useState([]);
+
+  const { usuario, firebase } = useContext(FirebaseContext);
+
+  useEffect(() => {
+    const obtenerProductos = () => {
+      firebase.db
+        .collection("productos")
+        .orderBy("creado", "desc")
+        .onSnapshot(handleSnapshot);
+    };
+
+    function handleSnapshot(snapshot) {
+      const productos = snapshot.docs.map((doc) => {
+        return {
+          id: doc.id,
+          ...doc.data(),
+        };
+      });
+
+      console.log(productos);
+
+      setProductos(productos);
+    }
+
+    obtenerProductos();
+  }, []);
+
   return (
-    <div >
+    <div>
       <Layout>
         <h1>INICIO</h1>
       </Layout>
-      
     </div>
-  )
+  );
 }
